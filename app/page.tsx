f24bdfd55b9129f61cb6210685fc339415f2e165
A S H <shadowash8@protonmail.com>
2026-04-27 10:52:51 +0530

    feat: add index page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
     return (
-        <div>
-            <h1>hi im shadowash</h1>
-        </div>
+        <section>
+            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+                A S H
+            </h1>
+            <p className="mb-4">
+                hey, just a random teenager who likes to code and tinker with linux. i just build software i need and for others
+            </p>
+        </section>
     );
 }
