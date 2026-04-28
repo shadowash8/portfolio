882734a8a41ba9f5bfb2b8b888cf50abd44c6573
A S H <shadowash8@protonmail.com>
2026-04-28 19:31:12 +0530

    fix: add margin on y axis

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
             lang="en"
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
-            <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-                <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            <body className="antialiased max-w-xl mx-4 my-8 lg:mx-auto">
+                <main className="flex-auto min-w-0 my-6 flex flex-col px-2 md:px-0">
                     <Navbar />
                     {children}
                 </main>
