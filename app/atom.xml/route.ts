d2c83299eb4723a3659483d55c251c66041ba873
A S H <shadowash8@protonmail.com>
2026-06-08 21:54:11 +0530

    fix: date parsing in atom route

--- app/atom.xml/route.ts
+++ app/atom.xml/route.ts
@@ -2,32 +2,32 @@ import { getAllPosts } from "@/app/lib/org";
 import { Feed } from 'feed'
 
 export async function GET() {
-  const posts = await getAllPosts()
+    const posts = await getAllPosts()
 
-  const feed = new Feed({
-    title: 'A S H',
-    description: 'my website',
-    id: 'https://shadowash8.netlify.app/',
-    link: 'https://shadowash8.netlify.app/',
-    feedLinks: {
-      atom: 'https://shadowash8.netlify.app/atom.xml',
-    },
-    author: {
-      name: 'shadowash8',
-    },
-  })
-
-  for (const post of posts) {
-    feed.addItem({
-      title: post.title,
-      id: `https://shadowash8.netlify.app/posts/${post.slug}`,
-      link: `https://shadowash8.netlify.app/posts/${post.slug}`,
-      description: post.description || '',
-      date: new Date(post.date),
+    const feed = new Feed({
+        title: 'A S H',
+        description: 'my website',
+        id: 'https://shadowash8.netlify.app/',
+        link: 'https://shadowash8.netlify.app/',
+        feedLinks: {
+            atom: 'https://shadowash8.netlify.app/atom.xml',
+        },
+        author: {
+            name: 'shadowash8',
+        },
     })
-  }
 
-  return new Response(feed.atom1(), {
-    headers: { 'Content-Type': 'application/atom+xml' },
-  })
+    for (const post of posts) {
+        feed.addItem({
+            title: post.title,
+            id: `https://shadowash8.netlify.app/posts/${post.slug}`,
+            link: `https://shadowash8.netlify.app/posts/${post.slug}`,
+            description: post.description || '',
+            date: new Date(post.date ?? new Date()),
+        })
+    }
+
+    return new Response(feed.atom1(), {
+        headers: { 'Content-Type': 'application/atom+xml' },
+    })
 }
